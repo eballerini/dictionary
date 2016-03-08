fa36e3a47b2859e269e7be84ce23e28948f6ef56
Emmanuel B <emmanuel.ballerini@gmail.com>
2016-03-07 22:08:10 -0500

    fixed problem that occured when loading translations - extra slash

--- src/main/webapp/scripts/components/entities/translation/translation.search.service.js
+++ src/main/webapp/scripts/components/entities/translation/translation.search.service.js
@@ -4,7 +4,7 @@ angular.module('dictionaryApp')
     .factory('TranslationSearch', function ($resource) {
         return $resource('api/_search/translations/:query', {}, {
             'query': { method: 'GET', isArray: true},
-            'find' : { method: 'GET', isArray: true, url: '/api/translations/words/:wordId/languages/:toLanguageId', 
+            'find' : { method: 'GET', isArray: true, url: 'api/translations/words/:wordId/languages/:toLanguageId', 
             	parameter: {wordId: '@wordId', toLanguageId: '@toLanguageId'}}
         });
     });

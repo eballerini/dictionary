50daf606af965c06eea67cf04c18591e4cb55553
Emmanuel B <emmanuel.ballerini@gmail.com>
2016-03-17 17:57:41 -0400

    updated DefaultDashboardService to find the number of words per language - still WIP

--- src/main/java/org/dictionary/service/DefaultDashboardService.java
+++ src/main/java/org/dictionary/service/DefaultDashboardService.java
@@ -6,13 +6,18 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.dictionary.api.DashboardAPI;
 import org.dictionary.api.LanguageAPI;
 import org.dictionary.api.StatAPI;
 import org.dictionary.domain.Language;
+import org.dictionary.domain.Word;
 import org.dictionary.repository.LanguageRepository;
+import org.dictionary.repository.search.WordSearchRepository;
 import org.dictionary.translator.LanguageTranslator;
 import org.dictionary.translator.StatTranslator;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +25,9 @@ import org.slf4j.LoggerFactory;
 public class DefaultDashboardService implements DashboardService {
 
     private final Logger log = LoggerFactory.getLogger(DefaultDashboardService.class);
+
+    @Inject
+    private WordSearchRepository wordSearchRepository;
 
     @Inject
     private LanguageRepository languageRepository;
@@ -43,12 +51,25 @@ public class DefaultDashboardService implements DashboardService {
 
         for (Language language: allLanguages) {
             LanguageAPI languageAPI = languageTranslator.toAPI(language);
-            StatAPI stat = statTranslator.toAPI(languageAPI, -1L);
+            StatAPI stat = statTranslator.toAPI(languageAPI, countWords(languageAPI.getId()));
             stats.add(stat);
         }
 
         dashboard.setStats(stats);
 
         return dashboard;
+    }
+
+    private long countWords(long languageId) {
+
+        // TODO quick and dirty eeww
+        // not efficient - need to include the aggregate into the query
+        QueryBuilder searchQuery = QueryBuilders.matchQuery("language.id", languageId);
+        log.debug("query: {}", searchQuery);
+        Iterable<Word> words = wordSearchRepository.search(searchQuery);
+        final MutableLong numWords = new MutableLong(0);
+        words.forEach(word -> numWords.increment());
+
+        return numWords.longValue();
     }
 }

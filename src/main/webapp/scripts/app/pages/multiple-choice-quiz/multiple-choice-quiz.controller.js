15c5a72963111576ad36929930bacb30e6bbbdf9
Emmanuel B <emmanuel.ballerini@gmail.com>
2016-04-09 17:09:28 -0400

    cleaned up MultipleChoiceQuizController

--- src/main/webapp/scripts/app/pages/multiple-choice-quiz/multiple-choice-quiz.controller.js
+++ src/main/webapp/scripts/app/pages/multiple-choice-quiz/multiple-choice-quiz.controller.js
@@ -3,10 +3,8 @@
 angular.module('dictionaryApp')
     .controller('MultipleChoiceQuizController', function ($scope, $state, $modal, Language, WordSearch, TranslationSearch, Tag, MultipleChoiceQuiz) {
 
-        // TODO clean
       
         $scope.languages = [];
-        // $scope.numWordsSeen = 0;
         $scope.tags = [];
         $scope.selected_num_words = 2;
         $scope.num_words = [2, 5, 10, 20];
@@ -36,6 +34,7 @@ angular.module('dictionaryApp')
 
             var tagId = $scope.tag == null ? null : $scope.tag.id;
 
+            console.log('$scope.selected_num_words: ' + $scope.selected_num_words);
             MultipleChoiceQuiz.query({fromLanguageId: $scope.from_language.id, 
                 toLanguageId: $scope.to_language.id, 
                 tagId: tagId, 
@@ -45,10 +44,7 @@ angular.module('dictionaryApp')
                 $scope.words = result.questions;
                 console.log($scope.words);
                 $scope.total_num_words = $scope.words.length;
-                // $scope.word = result;
                 $scope.loaded = true;
-                // $scope.errorLoading = false;
-                // findTranslations($scope.word.id, $scope.to_language.id);
             }, function(response) {
                 alert('error');
                 if(response.status === 404) {
@@ -57,46 +53,7 @@ angular.module('dictionaryApp')
                     console.log('no words found');
                 }
             });
-            // $scope.translations = null;
-            // $scope.show = false;
-            // $scope.numWordsSeen = $scope.numWordsSeen + 1;
-
-            // var tagId = $scope.tag == null ? null : $scope.tag.id;
-
-            // WordSearch.findRandom({languageId: $scope.from_language.id, tagId: tagId}, function(result) {
-            //     $scope.word = result;
-            //     $scope.loaded = true;
-            //     $scope.errorLoading = false;
-            //     findTranslations($scope.word.id, $scope.to_language.id);
-            // }, function(response) {
-            //     if(response.status === 404) {
-            //         $scope.loaded = false;
-            //         $scope.errorLoading = true;
-            //         console.log('no words found');
-            //     }
-            // });            
-
         }
-
-        // $scope.showTranslations = function () {
-        //     $scope.show = true;
-        // }
-
-        // $scope.hideTranslations = function () {
-        //     $scope.show = false;
-        // }
-
-        // function findTranslations(wordId, toLanguageId) {
-        //     TranslationSearch.find({wordId: wordId, toLanguageId: toLanguageId}, function(result) {
-        //         $scope.translations = result;
-        //         $scope.translationsReady = true;
-        //     }, function(response) {
-        //         if(response.status === 404) {
-        //             $scope.translationsReady = false;
-        //             console.log('could not load translations');
-        //         }
-        //     });
-        // }
 
         $scope.submit = function() {
             alert('TODO');
@@ -117,7 +74,6 @@ angular.module('dictionaryApp')
         }
 
         $scope.reset = function() {
-            // $scope.numWordsSeen = 0;
             $scope.loaded = false;
             $scope.show = false;
             $scope.translations = null;

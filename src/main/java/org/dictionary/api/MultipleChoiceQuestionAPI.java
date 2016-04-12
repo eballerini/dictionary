60cfdb654ac2c7b3f36faec57cd6d9d10b0202d7
Emmanuel B <emmanuel.ballerini@gmail.com>
2016-04-12 08:42:33 -0400

    added correct answer to MultipleChoiceQuestionAPI

--- src/main/java/org/dictionary/api/MultipleChoiceQuestionAPI.java
+++ src/main/java/org/dictionary/api/MultipleChoiceQuestionAPI.java
@@ -7,6 +7,7 @@ public class MultipleChoiceQuestionAPI {
     private WordAPI word;
     private Set<WordAPI> answers;
     private Long answerWordId;
+    private Long correctAnswerWordId;
 
     public WordAPI getWord() {
         return word;
@@ -32,12 +33,21 @@ public class MultipleChoiceQuestionAPI {
         this.answers = answers;
     }
 
+    public Long getCorrectAnswerWordId() {
+        return correctAnswerWordId;
+    }
+
+    public void setCorrectAnswerWordId(Long correctAnswerWordId) {
+        this.correctAnswerWordId = correctAnswerWordId;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((answerWordId == null) ? 0 : answerWordId.hashCode());
         result = prime * result + ((answers == null) ? 0 : answers.hashCode());
+        result = prime * result + ((correctAnswerWordId == null) ? 0 : correctAnswerWordId.hashCode());
         result = prime * result + ((word == null) ? 0 : word.hashCode());
         return result;
     }
@@ -61,6 +71,11 @@ public class MultipleChoiceQuestionAPI {
                 return false;
         } else if (!answers.equals(other.answers))
             return false;
+        if (correctAnswerWordId == null) {
+            if (other.correctAnswerWordId != null)
+                return false;
+        } else if (!correctAnswerWordId.equals(other.correctAnswerWordId))
+            return false;
         if (word == null) {
             if (other.word != null)
                 return false;
@@ -72,7 +87,7 @@ public class MultipleChoiceQuestionAPI {
     @Override
     public String toString() {
         return "MultipleChoiceQuestionAPI [word=" + word + ", answers=" + answers + ", answerWordId=" + answerWordId
-                + "]";
+                + ", correctAnswerWordId=" + correctAnswerWordId + "]";
     }
 
 }

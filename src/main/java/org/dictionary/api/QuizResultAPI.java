80c2f2217798283a851c98dc0ef74984e5908370
Emmanuel B <emmanuel.ballerini@gmail.com>
2016-06-13 07:58:25 -0400

    added QuizResultAPI and TagAPI

---
+++ src/main/java/org/dictionary/api/QuizResultAPI.java
@@ -0,0 +1,87 @@
+package org.dictionary.api;
+
+import java.time.LocalDate;
+
+public class QuizResultAPI {
+
+    private Long id;
+    private int numWords;
+    private int numCorrectAnswers;
+    private LanguageAPI fromLanguage;
+    private LanguageAPI toLanguage;
+    private TagAPI tagAPI;
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumWords() {
+        return numWords;
+    }
+
+    public void setNumWords(int numWords) {
+        this.numWords = numWords;
+    }
+
+    public int getNumCorrectAnswers() {
+        return numCorrectAnswers;
+    }
+
+    public void setNumCorrectAnswers(int numCorrectAnswers) {
+        this.numCorrectAnswers = numCorrectAnswers;
+    }
+
+    public LanguageAPI getFromLanguage() {
+        return fromLanguage;
+    }
+
+    public void setFromLanguage(LanguageAPI fromLanguage) {
+        this.fromLanguage = fromLanguage;
+    }
+
+    public LanguageAPI getToLanguage() {
+        return toLanguage;
+    }
+
+    public void setToLanguage(LanguageAPI toLanguage) {
+        this.toLanguage = toLanguage;
+    }
+
+    public TagAPI getTag() {
+        return tagAPI;
+    }
+
+    public void setTag(TagAPI tagAPI) {
+        this.tagAPI = tagAPI;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public TagAPI getTagAPI() {
+        return tagAPI;
+    }
+
+    public void setTagAPI(TagAPI tagAPI) {
+        this.tagAPI = tagAPI;
+    }
+
+    @Override
+    public String toString() {
+        return "QuizResultAPI [id=" + id + ", numWords=" + numWords + ", numCorrectAnswers=" + numCorrectAnswers
+                + ", fromLanguage=" + fromLanguage + ", toLanguage=" + toLanguage + ", tagAPI=" + tagAPI + ", date="
+                + date + "]";
+    }
+
+
+}
---
+++ src/main/java/org/dictionary/api/TagAPI.java
@@ -0,0 +1,54 @@
+package org.dictionary.api;
+
+import java.util.Objects;
+
+public class TagAPI {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TagAPI tagAPI = (TagAPI) o;
+
+        if ( ! Objects.equals(id, tagAPI.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "TagAPI{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            '}';
+    }
+}
